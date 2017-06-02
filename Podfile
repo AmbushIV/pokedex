bf42cadbb27df0f4d74717542b47146b9be4e579
AmbushIV <ionut.demeterca94@gmail.com>
2017-06-02 03:10:45 +0300

    Finished the Pokedex App, updates to follow

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+use_frameworks!
+
+target 'pokedex' do
+  pod 'Alamofire', '~> 4.4'
+end
---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Ionut Demeterca on 02/06/2017.
+//  Copyright © 2017 Ionut Demeterca. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
